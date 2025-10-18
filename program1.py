58a19b5b4bcf863460e869b431dd719431f414b7
swathihosagpudr <swathihosagoudr@gamil.com>
2025-10-18 11:34:43 +0530

    save the modifyied program

--- program1.py
+++ program1.py
@@ -1,3 +1,9 @@
 a = float(input("enter the first number:"))
 b = float(input("enetr the secand number:"))
 print("addition:", a+b)
+print("substraction:",a-b)
+print("multiplication:", a*b)
+if b ! = 0
+print("division:", a/b)
+else:
+print("it will not division by zero!")
